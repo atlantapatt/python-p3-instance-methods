94f548243fdafa5a01cfae43620dab8e5e7495ce
AtlantaPatterson <atlantarpatterson@gmail.com>
2023-06-05 13:24:58 -0700

    done

--- lib/dog.py
+++ lib/dog.py
@@ -4,4 +4,9 @@ class Dog:
     # Class body goes here
 
     #Instance method definition
+    def sit(self):
+        print("The dog is sitting.")
+
+    def bark(self):
+        print("Woof!")
     pass
